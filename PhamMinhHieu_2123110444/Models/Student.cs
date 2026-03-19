9f3e3950b488374cfc91e07c3b35ea0e9d58c31c
Admin <Admin@D20806>
2026-03-19 14:16:29 +0700

    pmh

---
+++ PhamMinhHieu_2123110444/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhamMinhHieu_2123110444.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string StudentCode { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; } = string.Empty;
+        public DateTime Birthday { get; set; }
+    }
+}
--- PhamMinhHieu_2123110444/Program.cs
+++ PhamMinhHieu_2123110444/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using PhamMinhHieu_2123110444.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
